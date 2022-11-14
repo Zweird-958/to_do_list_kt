1cb5359422a0bb3aa7c6759465be32e01019211b
Zweird-958 <xdfamilyytt@gmail.com>
2022-11-14 09:13:13 +0100

    update

--- cod_javafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.cod_javafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.cod_javafx to javafx.fxml;
-    exports com.example.cod_javafx;
-}
